a3e9a0a3dbf64ad2f55316eae98fd8b1fd8f296b
Ryan Hung <ryanhungryan@gmail.com>
2024-06-22 13:13:49 +0100

    feat: config zellij and remove startship

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,6 @@ if status is-interactive
 end
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
-starship init fish | source
 zoxide init fish | source
 alias  ls "eza --long --color --icons --all " 
 alias cat "bat"
@@ -11,6 +10,13 @@ alias cat "bat"
 
 # path
 fish_add_path -g /home/ryanf/.cargo/bin
+
+#zellij
+set -gx ZELLIJ_RUNNER_ROOT_DIR Dev
+set -gx ZELLIJ_RUNNER_IGNORE_DIRS node_modules,target
+set -gx ZELLIJ_RUNNER_MAX_DIRS_DEPTH 3
+set -gx ZELLIJ_RUNNER_LAYOUT_DIR .config/zellij/layouts
+set -gx ZELLIJ_RUNNER_BANNERS_DIR .config/zellij/banners
 
 set -gx EDITOR nvim
 
